fd91b56885ad10fb514e10b8c8e200c740253728
pzm <717864916@qq.com>
2019-01-03 22:55:00 +0800

    添加了 icon和iconPosition：支持自定义图标（enum）可选择图标位置

--- src/button.vue
+++ src/button.vue
@@ -1,18 +1,29 @@
 <template>
-    <button class="g-button">确定d</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href="`#i-${icon}`">
+            </use>
+        </svg>
+        <slot class="button-name"></slot>
+    </button>
 </template>
 <script>
     export default {
+        props:['icon','iconPosition']
     }
 </script>
 <style rel="stylesheet/scss" type="text/scss" lang="scss">
     .g-button{
         font-size:var(--base-font-size);
         height:var(--base-height);
-        padding:0 1em;
+        padding:0 0.1em;
         border-radius:var(--border-radius);
         border:1px solid var(--border-color);
         background-color:var(--button-bg);
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        vertical-align: top;        //避免inline不对齐
         &:hover{
             border-color:var(--border-color-hover)
         }
@@ -21,6 +32,30 @@
         }
         &:focus{
             outline:none;
+        }
+        .icon {
+            width: 1em;
+            height: 1em;
+        }
+        &.icon-left{
+            .icon{
+                order:0;
+                margin-right:.3em;
+                margin-left:0;
+            }
+            .button-name{
+                order:1;
+            }
+        }
+        &.icon-right{
+            .icon{
+                order:1;
+                margin-right:0;
+                margin-left:.3em;
+            }
+            .button-name{
+                order:0;
+            }
         }
     }
 </style>
