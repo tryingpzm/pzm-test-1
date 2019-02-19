c59e69e803c418a72d8ad3ccf6cda4882fc541d8
pzm <717864916@qq.com>
2019-02-19 23:16:54 +0800

    改为引用parcel打包后的dist/index.js(避免由于node不支持import语法导致的错误)

---
+++ index.js
@@ -0,0 +1,9 @@
+//package.json - main 中定义本包入口为 index.js，在本js中导出文件
+
+import Button from "./src/Button"
+import ButtonGroup from "./src/ButtonGroup"
+import Icon from "./src/Icon"
+
+export {
+    ButtonGroup,Button,Icon
+}
